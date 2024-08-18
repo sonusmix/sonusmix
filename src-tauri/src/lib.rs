f5fa0d8601c1d94370bf42256295d228bcd79ec1
Keegan Conlee <dacid44@gmail.com>
2024-08-18 13:57:58 -0500

    refactor: SIMPLIFY SIMPLIFY SIMPLIFY Restart frontend with modern Tauri and SvelteKit, move backend crate into frontend tree, and refactor into one crate

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,4 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+// mod pipewire_api; // Does not compile yet, needs some updating to work with more recent versions of tokio and pipewire
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -9,8 +6,10 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
